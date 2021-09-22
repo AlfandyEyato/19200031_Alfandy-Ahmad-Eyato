794e1ad01e5f627014be5ca3ecdb03f527adbb78
AlfandyEyato <91172616+AlfandyEyato@users.noreply.github.com>
2021-09-22 11:36:01 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index() 
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Alfandy Ahmad Eyato
+         Saya tingga di daerah Ampama
+         olah raga yang saya sukai adalah
+         bola kaki";
+    }
+}
